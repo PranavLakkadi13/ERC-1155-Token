29d4b77b5a6e404f18ba4f5db91db4a3a8cd9f21
Pranav <pranavlakkadi@gmail.com>
2024-11-02 22:16:51 +0530

    next commit

--- test/ERC1155.t.sol
+++ test/ERC1155.t.sol
@@ -1,1 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.27;
 
+import { MyMultiToken } from "../src/ERC1155.sol";
+import { Test } from "forge-std/Test.sol";
+
+contract MyMultiTokenTest is Test, MyMultiToken {
+    
+}   
